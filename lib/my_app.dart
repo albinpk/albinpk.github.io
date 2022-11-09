dcb6b4fff50efdcc7e054fae1e4be447dc6e171a
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-09 17:21:21 +0530

    refactor: remove default app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Albin PK',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
     );
   }
 }
