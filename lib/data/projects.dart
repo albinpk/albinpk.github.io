089544774d63e428491d690d36668222f28d3c5e
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-10 15:37:26 +0530

    refactor: create projects list

---
+++ lib/data/projects.dart
@@ -0,0 +1,19 @@
+import '../models/project_model.dart';
+
+const List<Project> projects = [
+  Project(
+    title: 'WhatsApp UI clone',
+    description: 'WhatsApp UI clone in Flutter.',
+    screenshots: [],
+    repoUrl:
+        'https://github.com/albinpk/flutter-clones/tree/master/000-whatsapp#readme',
+  ),
+  Project(
+    title: 'BookMyShow Tracker',
+    description: 'When ticket sales for a particular movie or movies'
+        'on bookmyshow.com begin, BookMyShow Tracker will let you know.',
+    screenshots: [],
+    repoUrl:
+        'https://github.com/albinpk/flutter-apps/tree/master/001-bookmyshow_tracker#readme',
+  ),
+];
