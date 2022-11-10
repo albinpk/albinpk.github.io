4b278fae920113b60f0561ba848fd3039fd0e96d
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-10 18:22:52 +0530

    fix: text overflow in `ProjectView`
    add padding to project details scroll view.

--- lib/views/project_view.dart
+++ lib/views/project_view.dart
@@ -32,101 +32,99 @@ class _ProjectViewState extends State<ProjectView> {
           bodyColor: Colors.white,
         );
 
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // title
-                  Text(
-                    widget.project.title,
-                    style: textTheme.headlineMedium!.copyWith(
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-
-                  // Description
-                  Text(
-                    widget.project.description,
-                    style: textTheme.titleSmall,
-                  ),
-                  const SizedBox(height: 20),
-
-                  // Features
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:
-                        widget.project.features.map(_featureToWidget).toList(),
-                  ),
-
-                  // Repository url
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      foregroundColor: Colors.black87,
-                    ),
-                    onPressed: () async {
-                      if (!await launchUrlString(widget.project.repoUrl)) {
-                        log("Can't open repository url!");
-                      }
-                    },
-                    child: const Text('Repository'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          // Screenshots PageView
-          Expanded(
-            child: Row(
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Previous button
-                IconButton(
-                  onPressed: () {
-                    _pageController.previousPage(
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                    );
-                  },
-                  color: Colors.white,
-                  icon: const Icon(Icons.arrow_back_ios),
-                ),
-
-                // Image
-                Expanded(
-                  child: PageView.builder(
-                    controller: _pageController,
-                    itemBuilder: (context, index) {
-                      return Image.asset(
-                        'assets/images/chat-room-dark.jpg',
-                      );
-                    },
+                // title
+                Text(
+                  widget.project.title,
+                  style: textTheme.headlineMedium!.copyWith(
+                    color: Colors.white,
                   ),
                 ),
+                const SizedBox(height: 20),
 
-                // Next button
-                IconButton(
-                  onPressed: () {
-                    _pageController.nextPage(
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.ease,
-                    );
+                // Description
+                Text(
+                  widget.project.description,
+                  style: textTheme.titleSmall,
+                ),
+                const SizedBox(height: 20),
+
+                // Features
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:
+                      widget.project.features.map(_featureToWidget).toList(),
+                ),
+
+                // Repository url
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black87,
+                  ),
+                  onPressed: () async {
+                    if (!await launchUrlString(widget.project.repoUrl)) {
+                      log("Can't open repository url!");
+                    }
                   },
-                  color: Colors.white,
-                  icon: const Icon(Icons.arrow_forward_ios),
+                  child: const Text('Repository'),
                 ),
               ],
             ),
           ),
-        ],
-      ),
+        ),
+
+        // Screenshots PageView
+        Expanded(
+          child: Row(
+            children: [
+              // Previous button
+              IconButton(
+                onPressed: () {
+                  _pageController.previousPage(
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.ease,
+                  );
+                },
+                color: Colors.white,
+                icon: const Icon(Icons.arrow_back_ios),
+              ),
+
+              // Image
+              Expanded(
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemBuilder: (context, index) {
+                    return Image.asset(
+                      'assets/images/chat-room-dark.jpg',
+                    );
+                  },
+                ),
+              ),
+
+              // Next button
+              IconButton(
+                onPressed: () {
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.ease,
+                  );
+                },
+                color: Colors.white,
+                icon: const Icon(Icons.arrow_forward_ios),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
@@ -160,11 +158,13 @@ class _ProjectViewState extends State<ProjectView> {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Bullet point
           Container(
             width: 8,
             height: 8,
+            margin: const EdgeInsets.only(top: 6),
             decoration: BoxDecoration(
               color: step ? Colors.white : null,
               shape: BoxShape.circle,
@@ -174,11 +174,13 @@ class _ProjectViewState extends State<ProjectView> {
           const SizedBox(width: 10),
 
           // Title
-          Text(
-            title,
-            style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                  color: Colors.white,
-                ),
+          Flexible(
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
           ),
         ],
       ),
