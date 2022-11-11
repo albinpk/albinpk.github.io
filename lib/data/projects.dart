25c993af8db9708e6e0a876b78c64ee5ea7f7f53
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-11 21:49:12 +0530

    refactor: add screenshots to projects data

--- lib/data/projects.dart
+++ lib/data/projects.dart
@@ -1,5 +1,8 @@
 import '../models/project_model.dart';
 
+const _assetsImageMobile = 'assets/images/mobile';
+const _assetsImageDesktop = 'assets/images/desktop';
+
 const List<Project> projects = [
   Project(
     title: 'WhatsApp UI clone',
@@ -26,13 +29,35 @@ const List<Project> projects = [
         Feature('Theme settings (with light and dark mode)'),
       ]),
     ],
-    screenshots: [],
+    screenshots: [
+      '$_assetsImageMobile/chat-room.gif',
+      '$_assetsImageMobile/home.gif',
+      '$_assetsImageMobile/settings.gif',
+      '$_assetsImageMobile/status.gif',
+      '$_assetsImageMobile/user-profile.gif',
+      '$_assetsImageDesktop/home-dark.png',
+      '$_assetsImageDesktop/home-light.png',
+      '$_assetsImageDesktop/chat-room-dark.png',
+      '$_assetsImageDesktop/chat-room-light.png',
+      '$_assetsImageDesktop/new-chat-dark.png',
+      '$_assetsImageDesktop/new-chat-light.png',
+      '$_assetsImageDesktop/profile-settings-dark.png',
+      '$_assetsImageDesktop/responsive-large.png',
+      '$_assetsImageDesktop/responsive-small.png',
+      '$_assetsImageDesktop/settings-dark.png',
+      '$_assetsImageDesktop/status-list-dark.png',
+      '$_assetsImageDesktop/status-screen-1.png',
+      '$_assetsImageDesktop/status-screen-2.png',
+      '$_assetsImageDesktop/theme-settings-light.png',
+      '$_assetsImageDesktop/user-profile-dark.png',
+      '$_assetsImageDesktop/user-profile-light.png',
+    ],
     repoUrl:
         'https://github.com/albinpk/flutter-clones/tree/master/000-whatsapp#readme',
   ),
   Project(
     title: 'BookMyShow Tracker',
-    description: 'When ticket sales for a particular movie or movies'
+    description: 'When ticket sales for a particular movie or movies '
         'on bookmyshow.com begin, BookMyShow Tracker will let you know.',
     features: [],
     screenshots: [],
